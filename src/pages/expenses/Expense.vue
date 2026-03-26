ad81998502d974e13d77b8b67556bacd494aff4c
arellanokaeresvil <arellanokaeresvil@gmail.com>
2026-03-26 15:30:53 +0800

    refactor: integrate expense summary, edit, delete action

--- src/pages/expenses/Expense.vue
+++ src/pages/expenses/Expense.vue
@@ -1,6 +1,6 @@
 <template>
-    <!-- <Skeleton /> -->
-    <div class="m-5">
+    <Skeleton v-if="expenses.loading" />
+    <div v-else class="m-5">
        <ContentHeader 
         title="Expense Management" 
         description="Track and manage all expenses" 
@@ -13,9 +13,9 @@
             <div class="flex-1 flex justify-between items-center center">
                 <div class="flex-1 h-10 -mt-5">
                     <div class="ml-2">
-                        <span class="text-white text-sm opacity-85 ">Total Expenses(March)</span>
+                        <span class="text-white text-sm opacity-85 ">Total Expenses({{ monthName }})</span>
                     </div> 
-                     <span class="text-4xl text-white inline-flex items-center opacity-90 font-extrabold"><PhilippinePeso size="35"/> 10,000</span>
+                     <span class="text-4xl text-white inline-flex items-center opacity-90 font-extrabold">{{ formatted( expenses.totalExpense) }}</span>
                 </div>
                 <div class="flex flex-1 h-10  justify-end pr-5">
                     <div class="rounded-xl glass w-15 h-15 text-white opacity-80 items-center flex justify-center -mt-2"><PhilippinePeso size="35"/></div>
@@ -29,40 +29,17 @@
         <div class=" grid grid-cols-1 lg:grid-cols-2 justify-between mt-5 gap-3">
             <div class=" flex-2">
                  <div class="flex items-center m-2">
-                    <h1 class="font-bold text-[13px]">Total expense by categories month of (MARCH)</h1>
+                    <h1 class="font-bold text-[13px]">Total expense by categories month of ({{ monthName }})</h1>
                 </div>
+                <NoData v-if="expenses.summary.length <= 0" />
+                <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
 
-                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
-                    <div class="border border-gray-300 border-t-7 border-t-gray-400 shadow-xl rounded-lg bg-white p-6">
-                        <h1 class="font-extrabold text-m mb-2">Feeds</h1>
-                        <h1 class="font-extrabold text-xl inline-flex items-center"><PhilippinePeso class="-ml-1" size="22"/>5,000</h1>
-                        <span class="block text-gray-500 text-sm">60.5% of total</span>
+                    <div v-for="item in expenses.summary" class="border border-gray-300 border-t-7 border-t-gray-400 shadow-xl rounded-lg bg-white p-6">
+                        <h1 class="font-extrabold text-m mb-2">{{ item.name }}</h1>
+                        <h1 class="font-extrabold text-xl inline-flex items-center">{{ formatted(item.total_amount) }}</h1>
+                        <span class="block text-gray-500 text-sm">{{ item.percentage }}% of total</span>
                     </div>
 
-                    <div class="border border-gray-300 border-t-7 border-t-gray-400 shadow-xl rounded-lg bg-white p-6">
-                        <h1 class="font-extrabold text-m mb-2">Supplements & Veterinary Drugs</h1>
-                        <h1 class="font-extrabold text-xl inline-flex items-center"><PhilippinePeso class="-ml-1" size="22"/>2,000</h1>
-                        <span class="block text-gray-500 text-sm">60.5% of total</span>
-                    </div>
-
-                    <div class="border border-gray-300 border-t-7 border-t-gray-400 shadow-xl rounded-lg bg-white p-6">
-                        <h1 class="font-extrabold text-m mb-2">Utilities</h1>
-                        <h1 class="font-extrabold text-xl inline-flex items-center"><PhilippinePeso class="-ml-1" size="22"/>1,000</h1>
-                        <span class="block text-gray-500 text-sm">60.5% of total</span>
-                    </div>
-
-                    <div class="border border-gray-300 border-t-7 border-t-gray-400 shadow-xl rounded-lg bg-white p-6">
-                        <h1 class="font-extrabold text-m mb-2">Maintenance</h1>
-                        <h1 class="font-extrabold text-xl inline-flex items-center"><PhilippinePeso class="-ml-1" size="22"/>1,000</h1>
-                        <span class="block text-gray-500 text-sm">60.5% of total</span>
-                    </div>
-
-                    <div class="border border-gray-300 border-t-7 border-t-gray-400 shadow-xl rounded-lg bg-white p-6">
-                        <h1 class="font-extrabold text-m mb-2">Labor</h1>
-                        <h1 class="font-extrabold text-xl inline-flex items-center"><PhilippinePeso class="-ml-1" size="22"/>1,000</h1>
-                        <span class="block text-gray-500 text-sm">60.5% of total</span>
-                    </div>
-                
 
                 </div>
 
@@ -73,9 +50,9 @@
 
                     <router-link to="/expenses/create-category" class="btn btn-black h-8 text-sm mr-2">Add Category</router-link>
                 </div>
-                <!-- <NoData /> -->
+              <NoData v-if="expenses.categories.length <= 0" />
 
-                      <div class="overflow-auto h-auto max-h-100">
+                      <div class="overflow-auto h-auto">
                        <table class="table table-s table-pin-rows table-pin-cols">
                         <thead>
                         <tr>
@@ -96,7 +73,16 @@
                         
                     </tbody>
                     </table>
-            </div>
+
+                    </div>
+
+                        <div class="flex justify-end">
+                                <div class="join">
+                                <button @click="categoryPaginate('prev')" class="join-item btn">«</button>
+                                <button class="join-item btn">{{ expenses.categories_pagination?.current_page }} of {{ expenses.categories_pagination?.last_page }}</button>
+                                <button @click="categoryPaginate('nxt')" class="join-item btn">»</button>
+                            </div>
+                        </div>
 
             </div>
 
@@ -112,13 +98,15 @@
                 <h1 class="font-bold text-[15px] ml-2">Recent Expenses</h1>
 
             </div>
-            <NoData v-show="expenses.data.length <= 0"  />
-            <div class="overflow-auto h-auto max-h-150">
-                       <table class="table table-s table-pin-rows table-pin-cols">
+            
+            <NoData v-if="expenses.data.length <= 0"  />
+            
+            <div v-else class=" h-auto max-h-155">
+                       <table class=" overflow-auto table table-s table-pin-rows table-pin-cols">
                         <thead>
                         <tr>
                             <td>Category</td>
-                            <td>Expense Date</td>
+                            <td class="cursor-pointer inline-flex items-center gap-1"><Calendar size="20" /> Expense Date</td>
                             <td>Reference/Receipt No.</td>
                             <td>Remarks</td>
                              <td>Amount</td>
@@ -134,7 +122,10 @@
                             <td>{{ item.reference_no }}</td>
                             <td>{{ item.description }}</td>
                             <td class="font-bold text-red-700 text-lg">{{ item.amount }}</td>
-                            <td class="inline-flex items-center gap-3"> <SquarePen class="cursor-pointer" size="20"/> <Trash2 class="text-red-600 cursor-pointer" size="20"/></td>
+                            <td class="inline-flex items-center gap-3"> 
+                                <router-link :to="`/expenses/edit/${item.id}`"> <SquarePen class="cursor-pointer" size="20"/></router-link>   
+                                <Trash2 @click="removeExpense(item.id)" class="text-red-600 cursor-pointer" size="20"/>
+                            </td>
                         </tr>
                        
 
@@ -142,6 +133,17 @@
 
                     </tbody>
                     </table>
+
+                <div class="flex justify-end">
+                        <div class="join">
+                        <button @click="recentPaginate('prev')" class="join-item btn">«</button>
+                        <button class="join-item btn">{{ expenses.data_pagination?.current_page }} of {{ expenses.data_pagination?.last_page }}</button>
+                        <button @click="recentPaginate('nxt')" class="join-item btn">»</button>
+                    </div>
+                </div>
+
+
+
             </div>
 
 
@@ -158,9 +160,65 @@ import { useExpenseStore } from '../../stores';
 import { onMounted } from 'vue';
 
 const expenses = useExpenseStore();
+const monthName = new Date().toLocaleString('en-US', { month: 'long' });
+
+const parameter = {
+    limit: 10,
+    page: 1,
+}
 
 onMounted( () => {
-    expenses.fetchExpenses()
-    expenses.fetchExpenseCategories()
+   index()
 })
+
+const index = () =>{
+    expenses.fetchExpenses(parameter)
+    expenses.fetchExpenseCategories(parameter)
+    expenses.fetchExpenseSummary()
+}
+
+const removeExpense = async (id:string) =>{
+    await expenses.deleteExpense(id)
+    index()
+}
+
+const formatted = (amount: string) => {
+    const formatPeso =  new Intl.NumberFormat('en-PH', {
+    style: 'currency',
+    currency: 'PHP'
+    }).format(amount);
+
+    return formatPeso;
+}
+
+
+const recentPaginate = async (action: string) =>{
+     let current_page = expenses.data_pagination?.current_page
+    let last_page = expenses.data_pagination?.last_page
+
+   let res = await paginate(action, current_page, last_page)
+   if(res) await expenses.fetchExpenses(parameter)
+}
+
+const categoryPaginate = async (action: string) =>{
+     let current_page = expenses.categories_pagination?.current_page
+    let last_page = expenses.categories_pagination?.last_page
+
+   let res = await paginate(action, current_page, last_page)
+   if(res) await expenses.fetchExpenseCategories(parameter)
+}
+
+const paginate = async (action: string, current_page: number, last_page: number) =>{
+
+    if (action === 'nxt') {
+        if (current_page >= last_page) return false; 
+        current_page += 1;
+    } else {
+        if (current_page <= 1) return false; 
+        current_page -= 1;
+    }
+   return  parameter.page = current_page
+   
+}
+
 </script>
